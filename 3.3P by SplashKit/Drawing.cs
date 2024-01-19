d807efb169fc794a28842c69a05636d77933121f
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-01-19 17:32:05 +1100

    Add files via upload

---
+++ 3.3P by SplashKit/Drawing.cs
@@ -0,0 +1,83 @@
+﻿using SplashKitSDK;
+using System.Collections.Generic;
+
+namespace ShapeDrawer
+{
+    public class Drawing
+    {
+        private readonly List<Shape> _shapes;
+        private Color _background;
+
+        public Drawing(Color background)
+        {
+            _shapes = new List<Shape>();
+            _background = background;
+        }
+
+        public Drawing() : this(Color.White)
+        {
+        }
+
+        public Color Background
+        {
+            get { return _background; }
+            set { _background = value; }
+        }
+
+        public int ShapeCount
+        {
+            get { return _shapes.Count; }
+        }
+
+        public List<Shape> SelectedShapes
+        {
+            get
+            {
+                List<Shape> selectedShapes = new List<Shape>();
+
+                foreach (Shape shape in _shapes)
+                {
+                    if (shape.Selected == true)
+                    {
+                        selectedShapes.Add(shape);
+                    }
+                }
+
+                return selectedShapes;
+            }
+        }
+
+
+        public void SelectShapesAt(Point2D pt)
+        {
+            foreach (Shape shape in _shapes)
+            {
+                shape.Selected = shape.IsAt(pt);
+            }
+        }
+
+        public void AddShape(Shape shape)
+        {
+            _shapes.Add(shape);
+        }
+
+        public void RemoveShape(Shape shape)
+        {
+            _shapes.Remove(shape);
+        }
+
+        /*public void ChangeBackgroundColor(Color color)
+        {
+            _background = color;
+        }*/
+
+        public void Draw()
+        {
+            SplashKit.ClearScreen(_background);
+            foreach (Shape shape in _shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
