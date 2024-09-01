97dd821c3ea198621c2728797815a0a6cc638701
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-09-01 16:33:24 +1000

    Add files via upload

---
+++ RandomGeneration/RandomGeneration/Options.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace RandomGeneration
+{
+	public class Options
+	{
+		private Random rand;
+        public int Value { get; private set; }
+		private bool tog = false;
+
+        public Options(int val)
+		{
+			rand = new Random();
+			Value = val;
+		}
+
+		public void RandomRoll()
+		{
+            Value = rand.Next(1, 7);
+        }
+
+		public void Choosen(int val)
+		{
+			if (!tog || tog)
+			{
+				if (val == 1)
+				{
+					Console.WriteLine("An Nha");
+                    tog = true;
+				}
+                else if (val == 2)
+                {
+                    Console.WriteLine("Pho Ganh");
+                    tog = true;
+                }
+                else if (val == 3)
+                {
+                    Console.WriteLine("Dragon HP");
+                    tog = true;
+                }
+                else if (val == 4)
+                {
+                    Console.WriteLine("Do Thai");
+                    tog = true;
+                }
+                else if (val == 5)
+                {
+                    Console.WriteLine("BBQ");
+                    tog = true;
+                }
+                else if (val == 6)
+                {
+                    Console.WriteLine("Doodee");
+                    tog = true;
+                }
+            }
+		}
+
+    }
+}
+
---
+++ RandomGeneration/RandomGeneration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RandomGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options opt = new Options(0);
+            bool eg = false;
+
+            while (!eg)
+            {
+                opt.RandomRoll();
+                opt.Choosen(opt.Value);
+            }
+        }
+    }
+}
